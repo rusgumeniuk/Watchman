e5b4df747a8e7ce944b1ce3e970891a85b399a4e
rusgumeniuk <rus.gumeniuk@gmail.com>
2020-03-16 23:29:18 +0200

    create model to work with password hashing

---
+++ Identity.API/Services/ICustomPasswordHasher.cs
@@ -0,0 +1,8 @@
+﻿namespace Identity.API.Services
+{
+    public interface ICustomPasswordHasher
+    {
+        string Hash(string password);
+        bool Verify(string hash, string password);
+    }
+}
