dc28a1622d9ae64d2309e780d891af1a98602e50
rusgumeniuk <rus.gumeniuk@gmail.com>
2020-03-21 14:02:02 +0200

    Create HealthMeasurementViewModel.cs

---
+++ HealthService.API/Models/HealthMeasurementViewModel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+using Watchman.BusinessLogic.Models.Signs;
+
+namespace HealthService.API.Models
+{
+    public class HealthMeasurementViewModel
+    {
+        [Required]
+        public Guid PatientId { get; set; }
+
+        
+        public IEnumerable<SignPair> Signs { get; set; }
+
+        public DateTime DateTime { get; set; } = DateTime.Now;
+    }
+    public class SignPair
+    {
+        [Required]
+        public string Type { get; set; }
+        [Required]
+        public int Value { get; set; }
+    }
+}
