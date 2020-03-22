8aab327f7aef96583f48946fdd128b2cfd404a4c
rusgumeniuk <rus.gumeniuk@gmail.com>
2020-03-22 13:44:12 +0200

    add method AnalyzeLast

--- Watchman.BusinessLogic/Models/Analysis/IAnalysisStrategy.cs
+++ Watchman.BusinessLogic/Models/Analysis/IAnalysisStrategy.cs
@@ -1,18 +1,19 @@
 ﻿using System;
 
 using Watchman.BusinessLogic.Models.Signs;
+using Watchman.BusinessLogic.Models.Users;
 
 namespace Watchman.BusinessLogic.Models.Analysis
 {
-    public interface IAnalysisStrategy : IAnalysisStrategy<Guid, Guid, Guid, Guid, Guid> { }
-    public interface IAnalysisStrategy<TStrategyKey, TResultKey, THealthMeasurementKey, TCallToActionKey, TSignsKey> : IIdentifiedEntity<TStrategyKey>
-        where TStrategyKey : IEquatable<TStrategyKey>
-        where TResultKey : IEquatable<TResultKey>
+    public interface IAnalysisStrategy : IAnalysisStrategy<Guid, Guid, Guid> { }
+    public interface IAnalysisStrategy<THealthMeasurementKey, TCallToActionKey, TSignsKey>
         where THealthMeasurementKey : IEquatable<THealthMeasurementKey>
         where TSignsKey : IEquatable<TSignsKey>
         where TCallToActionKey : IEquatable<TCallToActionKey>
     {
-        IAnalysisResult<TResultKey, THealthMeasurementKey, TCallToActionKey, TSignsKey> Analyze<TPatientKey>(HealthMeasurement<THealthMeasurementKey, TSignsKey> healthState, TPatientKey patientId)
+        IAnalysisResult<THealthMeasurementKey, TCallToActionKey, TSignsKey> AnalyzeLast<TPatientKey>(Patient<TPatientKey> patient)
+            where TPatientKey : IEquatable<TPatientKey>;
+        IAnalysisResult<THealthMeasurementKey, TCallToActionKey, TSignsKey> Analyze<TPatientKey>(HealthMeasurement<THealthMeasurementKey, TSignsKey> healthState, Patient<TPatientKey> patient)
             where TPatientKey : IEquatable<TPatientKey>;
     }
 }
--- Watchman.BusinessLogic/Models/Analysis/IHealthAnalyzer.cs
+++ Watchman.BusinessLogic/Models/Analysis/IHealthAnalyzer.cs
@@ -1,22 +1,22 @@
 ﻿using System;
 
 using Watchman.BusinessLogic.Models.Signs;
+using Watchman.BusinessLogic.Models.Users;
 
 namespace Watchman.BusinessLogic.Models.Analysis
 {
-    public interface IHealthAnalyzer : IHealthAnalyzer<Guid, Guid, Guid, Guid, Guid, Guid> { }
-    public interface IHealthAnalyzer<TAnalyzerKey, TResultKey, TStrategyKey, THealthMeasurementKey, TCallToActionKey, TSignsKey> : IIdentifiedEntity<TAnalyzerKey>
-        where TAnalyzerKey : IEquatable<TAnalyzerKey>
-        where TResultKey : IEquatable<TResultKey>
-        where TStrategyKey : IEquatable<TStrategyKey>
+    public interface IHealthAnalyzer : IHealthAnalyzer<Guid, Guid, Guid> { }
+    public interface IHealthAnalyzer<THealthMeasurementKey, TCallToActionKey, TSignsKey>         
         where THealthMeasurementKey : IEquatable<THealthMeasurementKey>
         where TCallToActionKey : IEquatable<TCallToActionKey>
         where TSignsKey : IEquatable<TSignsKey>
     {
-        IAnalysisStrategy<TStrategyKey, TResultKey, THealthMeasurementKey, TCallToActionKey, TSignsKey> AnalysisStrategy { get; set; }
-        IAnalysisResult<TResultKey, THealthMeasurementKey, TCallToActionKey, TSignsKey> AnalysisResult { get; set; }
+        IAnalysisStrategy<THealthMeasurementKey, TCallToActionKey, TSignsKey> AnalysisStrategy { get; set; }
+        IAnalysisResult<THealthMeasurementKey, TCallToActionKey, TSignsKey> AnalysisResult { get; set; }
 
-        void Analyze<TPatientKey>(HealthMeasurement<THealthMeasurementKey, TSignsKey> healthState, TPatientKey patientId)
+        void AnalyzeLast<TPatientKey>(Patient<TPatientKey> patient)
+            where TPatientKey : IEquatable<TPatientKey>;
+        void Analyze<TPatientKey>(HealthMeasurement<THealthMeasurementKey, TSignsKey> healthState, Patient<TPatientKey> patient)
             where TPatientKey : IEquatable<TPatientKey>;
     }
 }
