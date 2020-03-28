2db51cfdb1d217dd75524a5ac6b68a383dee1fcc
rusgumeniuk <rus.gumeniuk@gmail.com>
2020-03-28 23:25:56 +0200

    create account controller with login and register actions, views/ create empty view models

---
+++ Watchman.Web/Controllers/AccountController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Watchman.Web.Models;
+
+namespace Watchman.Web.Controllers
+{
+    public class AccountController : Controller
+    {
+        [HttpGet]
+        public IActionResult Login()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Login(LoginViewModel viewModel)
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Register()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Register(RegisterViewModel viewModel)
+        {
+            return View();
+        }
+    }
+}
