de2d8abfb4a2c534fe91cd46c23b60a39a813360
rusgumeniuk <rus.gumeniuk@gmail.com>
2020-04-16 22:30:29 +0300

    add token parameter to all methods

--- Watchman.BusinessLogic/Services/IWatchmanPatientService.cs
+++ Watchman.BusinessLogic/Services/IWatchmanPatientService.cs
@@ -11,22 +11,25 @@ namespace Watchman.BusinessLogic.Services
     public interface IWatchmanPatientService<TKey>
         where TKey : IEquatable<TKey>
     {
-        Task<IAnalysisResult> AnalyzeLastMeasurementAsync(TKey patientId);
+        Task<IAnalysisResult> AnalyzeLastMeasurementAsync(TKey patientId, string token = null);
 
-        Task<HealthMeasurement<TKey, TKey>> GetLastHealthMeasurementAsync(TKey patientId);
-        Task<IEnumerable<HealthMeasurement<TKey, TKey>>> GetLastHealthMeasurementsAsync(TKey patientId, int count);
-        Task AddHealthMeasurementAsync(TKey patientId, HealthMeasurement<TKey, TKey> healthMeasurement);
+        Task<HealthMeasurement<TKey, TKey>> GetLastHealthMeasurementAsync(TKey patientId, string token = null);
+        Task<IEnumerable<HealthMeasurement<TKey, TKey>>> GetLastHealthMeasurementsAsync(TKey patientId, int count, string token = null);
+        Task AddHealthMeasurementAsync(TKey patientId, HealthMeasurement<TKey, TKey> healthMeasurement, string token = null);
 
-        Task AddIgnorableSignToPatientAsync(TKey patientId, Sign<TKey> sign);
+        Task AddIgnorableSignToPatientAsync(TKey patientId, Sign<TKey> sign, string token = null);
 
-        Task AddPatientToWatchmanAsync(TKey watchmanId, TKey patientId);
+        Task AddPatientToWatchmanAsync(TKey watchmanId, TKey patientId, string token = null);
 
-        Task RemovePatientFromWatchmanAsync(TKey watchmanId, TKey patientId);
+        Task RemovePatientFromWatchmanAsync(TKey watchmanId, TKey patientId, string token = null);
 
-        Task CreatePatient(Patient<TKey> patient, string token = null);
-        Task CreateWatchman(WatchmanProfile<TKey> watchman);
+        Task CreatePatientAsync(Patient<TKey> patient, string token = null);
+        Task CreateWatchmanAsync(WatchmanProfile<TKey> watchman, string token = null);
 
-        void RemoveAllWatchmenFromPatient(TKey patientId);
-        void RemoveAllPatientFromWatchman(TKey watchmanId);
+        Task<Patient<TKey>> GetPatientAsync(TKey id, string token = null);
+        Task<WatchmanProfile<TKey>> GetWatchmanAsync(TKey id, string token = null);
+
+        void RemoveAllWatchmenFromPatient(TKey patientId, string token = null);
+        void RemoveAllPatientFromWatchman(TKey watchmanId, string token = null);
     }
 }
