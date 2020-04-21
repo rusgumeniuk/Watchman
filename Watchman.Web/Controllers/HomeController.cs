d20ee915759eeb78aa4597ab9d8e7aa79ab8703b
rusgumeniuk <rus.gumeniuk@gmail.com>
2020-04-21 14:23:58 +0300

    add PatientProfile action

--- Watchman.Web/Controllers/HomeController.cs
+++ Watchman.Web/Controllers/HomeController.cs
@@ -4,7 +4,9 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net.Http;
+using System.Security.Claims;
 using System.Threading.Tasks;
+using Watchman.BusinessLogic.Models.PatientStates.ActivityStates;
 using Watchman.BusinessLogic.Models.Users;
 using Watchman.BusinessLogic.Services;
 using Watchman.Web.Models;
@@ -13,12 +15,18 @@ namespace Watchman.Web.Controllers
 {    
     public class HomeController : Controller
     {
-        private readonly IUserManager<WatchmanUser, Guid> userManager;
-        private readonly IWatchmanPatientService<Guid> watchmanPatientService;
-        public HomeController(IUserManager<WatchmanUser, Guid> userManager, IWatchmanPatientService<Guid> watchmanPatientService)
+        private readonly IUserManager<WatchmanUser, Guid> _userManager;
+        private readonly IWatchmanPatientService<Guid> _watchmanPatientService;
+        private readonly IUserWatchmanPatientService<Guid> _userHealthService;
+        public HomeController(
+            IUserManager<WatchmanUser, Guid> userManager,
+            IWatchmanPatientService<Guid> watchmanPatientService,
+            IUserWatchmanPatientService<Guid> userWatchmanPatientService
+            )
         {
-            this.userManager = userManager;
-            this.watchmanPatientService = watchmanPatientService;
+            this._userManager = userManager;
+            this._watchmanPatientService = watchmanPatientService;
+            this._userHealthService = userWatchmanPatientService;
         }
         public IActionResult Index()
         {           
@@ -30,9 +38,29 @@ namespace Watchman.Web.Controllers
         {
             var token = Request.Cookies["access_token"];
             var email = User.Claims.FirstOrDefault(claim => claim.Equals("email") || claim.Type.Contains("email"))?.Value;
-            var user = userManager.FindByEmailAsync(email, token).Result;
-            WatchmanProfile profile = await watchmanPatientService.GetWatchmanWithPropertiesByUserIdAsync(user.Id);          
-            return View(profile);
+            var user = _userManager.FindByEmailAsync(email, token).Result;
+            Patient newPatient = new Patient()
+            {
+                Id = Guid.NewGuid(),
+                CurrentActivityState = new SleepActivityState()
+
+            };
+            await _watchmanPatientService.CreatePatientAsync(newPatient, token);
+            await _userHealthService.AddPatientToUserAsync(user.Id, newPatient.Id, token);
+            //WatchmanProfile profile = await watchmanPatientService.GetWatchmanWithPropertiesByUserIdAsync(user.Id);          
+            return View(newPatient);
+        }
+
+        [Authorize]
+        public async Task<IActionResult> PatientProfile(string value = null)
+        {
+            var token = Request.Cookies["access_token"];
+            string stringId = value ?? User.FindFirstValue("patientIdClaim");
+            var id = Guid.Parse(stringId);
+
+            var patient = await _watchmanPatientService.GetPatientAsync(id, token);
+
+            return View(patient);
         }
         
     }
