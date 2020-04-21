8e9c1230965dc691efdebc79617a89da52a8a974
rusgumeniuk <rus.gumeniuk@gmail.com>
2020-04-21 19:27:48 +0300

    add opportunity to create Patient and Watchman profile if doesn't exist

--- Watchman.Web/Controllers/HomeController.cs
+++ Watchman.Web/Controllers/HomeController.cs
@@ -1,18 +1,17 @@
 ﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Net.Http;
 using System.Security.Claims;
 using System.Threading.Tasks;
+
 using Watchman.BusinessLogic.Models.PatientStates.ActivityStates;
-using Watchman.BusinessLogic.Models.Users;
 using Watchman.BusinessLogic.Services;
+using Watchman.Web.Extensions;
 using Watchman.Web.Models;
 
 namespace Watchman.Web.Controllers
-{    
+{
     public class HomeController : Controller
     {
         private readonly IUserManager<WatchmanUser, Guid> _userManager;
@@ -28,40 +27,82 @@ namespace Watchman.Web.Controllers
             this._watchmanPatientService = watchmanPatientService;
             this._userHealthService = userWatchmanPatientService;
         }
+
+        [HttpGet]
         public IActionResult Index()
-        {           
+        {
             return View();
         }
 
         [Authorize]
-        public async Task<IActionResult> WatchmanProfile()
+        [HttpPost]
+        public async Task<IActionResult> CreateWatchmanProfile()
         {
-            var token = Request.Cookies["access_token"];
-            var email = User.Claims.FirstOrDefault(claim => claim.Equals("email") || claim.Type.Contains("email"))?.Value;
-            var user = _userManager.FindByEmailAsync(email, token).Result;
-            Patient newPatient = new Patient()
-            {
-                Id = Guid.NewGuid(),
-                CurrentActivityState = new SleepActivityState()
+            var token = this.GetAccessTokenFromCookies();
+            var email = this.GetUserEmailFromHttpContext();
 
-            };
-            await _watchmanPatientService.CreatePatientAsync(newPatient, token);
-            await _userHealthService.AddPatientToUserAsync(user.Id, newPatient.Id, token);
-            //WatchmanProfile profile = await watchmanPatientService.GetWatchmanWithPropertiesByUserIdAsync(user.Id);          
-            return View(newPatient);
+            var user = _userManager.FindByEmailAsync(email, token).Result;
+            WatchmanInfo newWatchman = new WatchmanInfo() { Id = Guid.NewGuid() };
+
+            await _watchmanPatientService.CreateWatchmanAsync(newWatchman, token);
+            await _userHealthService.AddWatchmanToUserAsync(user.Id, newWatchman.Id, token);
+            return RedirectToAction("WatchmanProfile");
         }
 
         [Authorize]
-        public async Task<IActionResult> PatientProfile(string value = null)
+        [HttpGet]
+        public async Task<IActionResult> WatchmanProfile()
         {
-            var token = Request.Cookies["access_token"];
-            string stringId = value ?? User.FindFirstValue("patientIdClaim");
+            string stringId = User.FindFirstValue("watchmanIdClaim");
+
+            if (String.IsNullOrWhiteSpace(stringId))
+            {
+                return RedirectToAction("RefreshToken", "Account", new { returnUrl = Request.Path.Value });
+            }
+
+            var token = this.GetAccessTokenFromCookies();
+            var id = Guid.Parse(stringId);
+
+            var watchman = await _watchmanPatientService.GetWatchmanAsync(id, token);
+
+            return View(watchman);
+        }
+
+        [Authorize]
+        [HttpPost]
+        public async Task<IActionResult> CreatePatientProfile()
+        {
+            var token = this.GetAccessTokenFromCookies();
+            var email = this.GetUserEmailFromHttpContext();
+
+            var user = _userManager.FindByEmailAsync(email, token).Result;
+            PatientInfo newPatientInfo = new PatientInfo()
+            {
+                Id = Guid.NewGuid(),
+                CurrentActivityState = new SleepActivityState()
+            };
+            await _watchmanPatientService.CreatePatientAsync(newPatientInfo, token);
+            await _userHealthService.AddPatientToUserAsync(user.Id, newPatientInfo.Id, token);
+            return RedirectToAction("PatientProfile");
+        }
+
+        [Authorize]
+        [HttpGet]
+        public async Task<IActionResult> PatientProfile()
+        {
+            string stringId = User.FindFirstValue("patientIdClaim");
+
+            if (String.IsNullOrWhiteSpace(stringId))
+            {
+                return RedirectToAction("RefreshToken", "Account", new { returnUrl = Request.Path.Value });
+            }
+
+            var token = this.GetAccessTokenFromCookies();
             var id = Guid.Parse(stringId);
 
             var patient = await _watchmanPatientService.GetPatientAsync(id, token);
 
             return View(patient);
         }
-        
     }
 }
