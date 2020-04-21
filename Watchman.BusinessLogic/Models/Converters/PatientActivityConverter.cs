8a73c3af1332cf42eddd58914906c893156c74af
rusgumeniuk <rus.gumeniuk@gmail.com>
2020-04-21 14:22:26 +0300

    create PatientActivityResolver and -Converter for exact deserialization to derived classes

---
+++ Watchman.BusinessLogic/Models/Converters/PatientActivityConverter.cs
@@ -0,0 +1,44 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+using System;
+
+using Watchman.BusinessLogic.Models.PatientStates.ActivityStates;
+
+namespace Watchman.BusinessLogic.Models.Converters
+{
+    public class PatientActivityConverter : JsonConverter
+    {
+        private static readonly JsonSerializerSettings _settings = new JsonSerializerSettings()
+        { ContractResolver = new PatientActivityResolver() };
+
+
+        public override object ReadJson(JsonReader reader, Type objectType, object existingValue, JsonSerializer serializer)
+        {
+            JObject jo = JObject.Load(reader);
+            switch ((float)jo.GetValue("changeFactor"))
+            {
+                case 1.2f:
+                    return JsonConvert.DeserializeObject<SleepActivityState>(jo.ToString(), _settings);
+                case 1f:
+                    return JsonConvert.DeserializeObject<CasualActivityState>(jo.ToString(), _settings);
+                case 0.75f:
+                    return JsonConvert.DeserializeObject<SportActivityState>(jo.ToString(), _settings);
+                default:
+                    throw new Exception();
+            }
+            throw new NotImplementedException();
+        }
+
+        public override bool CanConvert(Type objectType)
+        {
+            return (objectType == typeof(PatientActivityState<Guid>));
+        }
+
+        public override bool CanWrite => false;
+        public override void WriteJson(JsonWriter writer, object value, JsonSerializer serializer)
+        {
+            throw new NotImplementedException();//canWrite => false so we can't write and don't need to implement
+        }
+    }
+}
--- Watchman.BusinessLogic/Models/PatientStates/ActivityStates/PatientActivityState.cs
+++ Watchman.BusinessLogic/Models/PatientStates/ActivityStates/PatientActivityState.cs
@@ -1,7 +1,12 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+
+using System;
+
+using Watchman.BusinessLogic.Models.Converters;
 
 namespace Watchman.BusinessLogic.Models.PatientStates.ActivityStates
 {
+    [JsonConverter(typeof(PatientActivityConverter))]
     public abstract class PatientActivityState<TKey> : IIdentifiedEntity<TKey>
         where TKey : IEquatable<TKey>
     {
