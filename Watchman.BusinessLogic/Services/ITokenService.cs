ffd34ab55017fc074cef1e8e6aa99c5bc11c0d6d
rusgumeniuk <rus.gumeniuk@gmail.com>
2020-04-21 17:25:57 +0300

    create RefreshToken

--- Watchman.BusinessLogic/Services/ITokenService.cs
+++ Watchman.BusinessLogic/Services/ITokenService.cs
@@ -5,5 +5,6 @@ namespace Watchman.BusinessLogic.Services
     public interface ITokenService
     {
         Task<string> GetTokenAsync(string email, string password);
+        Task<string> RefreshToken(string email, string oldToken = null);
     }
 }
--- Watchman.Web/Services/TokenService.cs
+++ Watchman.Web/Services/TokenService.cs
@@ -1,5 +1,4 @@
-﻿using Newtonsoft.Json;
-
+﻿
 using System;
 using System.Net.Http;
 using System.Threading.Tasks;
@@ -11,7 +10,7 @@ namespace Watchman.Web.Services
 {
     public class TokenService : ITokenService
     {
-        private const string loginUrl = "https://localhost:44383/account";
+        private const string AccountUrl = "https://localhost:44383/account";
         private readonly IHttpClient client;
 
         public TokenService(IHttpClient httpClient)
@@ -21,18 +20,26 @@ namespace Watchman.Web.Services
 
         public async Task<string> GetTokenAsync(string email, string password)
         {
-            var uri = loginUrl + "/login";
+            var uri = AccountUrl + "/login";
             var obj = new LoginViewModel() { Email = email, Password = password };
 
             var response = await client.SendRequest(HttpMethod.Post, null, obj, uri);
             var result = await client.GetResponseResult(response);
 
             return !response.IsSuccessStatusCode || String.IsNullOrWhiteSpace(result)
-                ? String.Empty : JsonConvert.DeserializeObject<TokenModel>(result).Token;
+                ? String.Empty : result;
         }
-    }
-    class TokenModel
-    {
-        public string Token { get; set; }
+
+        public async Task<string> RefreshToken(string email, string oldToken = null)
+        {
+            var uri = AccountUrl + "/RefreshToken";
+            var obj = new { Email = email };
+
+            var response = await client.SendRequest(HttpMethod.Post, null, obj, uri, oldToken);
+            var result = await client.GetResponseResult(response);
+
+            return !response.IsSuccessStatusCode || String.IsNullOrWhiteSpace(result)
+                ? String.Empty : result;
+        }
     }
 }
