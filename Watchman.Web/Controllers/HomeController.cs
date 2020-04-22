c4dc6d767f464f327576eccde6c9a3ad0cf352e6
rusgumeniuk <rus.gumeniuk@gmail.com>
2020-04-22 19:11:34 +0300

    add methods SendRequestToPatient, Accept\RefuseRequest, change Patient\WatchmanProfile

--- Watchman.Web/Controllers/HomeController.cs
+++ Watchman.Web/Controllers/HomeController.cs
@@ -2,39 +2,47 @@
 using Microsoft.AspNetCore.Mvc;
 
 using System;
+using System.Collections.Generic;
 using System.Security.Claims;
 using System.Threading.Tasks;
 
 using Watchman.BusinessLogic.Models.PatientStates.ActivityStates;
+using Watchman.BusinessLogic.Models.Users;
 using Watchman.BusinessLogic.Services;
 using Watchman.Web.Extensions;
 using Watchman.Web.Models;
+using Watchman.Web.ViewModels;
 
 namespace Watchman.Web.Controllers
 {
+    [Authorize]
     public class HomeController : Controller
     {
+        private readonly IPersonalInformationService<PersonalInfo, Guid> _personalService;
         private readonly IUserManager<WatchmanUser, Guid> _userManager;
         private readonly IWatchmanPatientService<Guid> _watchmanPatientService;
         private readonly IUserWatchmanPatientService<Guid> _userHealthService;
+        private readonly IControlRequestService _controlRequestService;
         public HomeController(
             IUserManager<WatchmanUser, Guid> userManager,
             IWatchmanPatientService<Guid> watchmanPatientService,
-            IUserWatchmanPatientService<Guid> userWatchmanPatientService
-            )
+            IUserWatchmanPatientService<Guid> userWatchmanPatientService,
+            IControlRequestService controlRequestService, IPersonalInformationService<PersonalInfo, Guid> personalService)
         {
             this._userManager = userManager;
             this._watchmanPatientService = watchmanPatientService;
             this._userHealthService = userWatchmanPatientService;
+            this._controlRequestService = controlRequestService;
+            _personalService = personalService;
         }
 
+        [AllowAnonymous]
         [HttpGet]
         public IActionResult Index()
         {
             return View();
         }
 
-        [Authorize]
         [HttpPost]
         public async Task<IActionResult> CreateWatchmanProfile()
         {
@@ -49,7 +57,7 @@ namespace Watchman.Web.Controllers
             return RedirectToAction("WatchmanProfile");
         }
 
-        [Authorize]
+
         [HttpGet]
         public async Task<IActionResult> WatchmanProfile()
         {
@@ -65,10 +73,10 @@ namespace Watchman.Web.Controllers
 
             var watchman = await _watchmanPatientService.GetWatchmanAsync(id, token);
 
-            return View(watchman);
+            return View(watchman as WatchmanInfo);
         }
 
-        [Authorize]
+
         [HttpPost]
         public async Task<IActionResult> CreatePatientProfile()
         {
@@ -86,11 +94,11 @@ namespace Watchman.Web.Controllers
             return RedirectToAction("PatientProfile");
         }
 
-        [Authorize]
+
         [HttpGet]
-        public async Task<IActionResult> PatientProfile()
+        public async Task<IActionResult> PatientProfile(string patientId = null)
         {
-            string stringId = User.FindFirstValue("patientIdClaim");
+            string stringId = patientId ?? User.FindFirstValue("patientIdClaim");
 
             if (String.IsNullOrWhiteSpace(stringId))
             {
@@ -101,8 +109,88 @@ namespace Watchman.Web.Controllers
             var id = Guid.Parse(stringId);
 
             var patient = await _watchmanPatientService.GetPatientAsync(id, token);
+            var pendingRequests = await _controlRequestService.GetPendingRequests(id, token);
+            IList<PersonalInfoRequestIdPair> pairs = new List<PersonalInfoRequestIdPair>();
+            foreach (var request in pendingRequests ?? new List<ControlRequest>())
+            {
+                var user = await _userManager.FindByWatchman(request.WatchmanId, token);
+                var info = await _personalService.GetPersonalInformation(user.PersonalInformationId, token);
+                pairs.Add(new PersonalInfoRequestIdPair(info, request.Id));
+            }
 
-            return View(patient);
+            PatientProfileViewModel model = new PatientProfileViewModel(patient, pairs);
+            return View(model);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> SendRequestToPatient(string patientEmail)
+        {
+            if (patientEmail.Equals(this.GetUserEmailFromHttpContext()))
+            {
+                ModelState.AddModelError("", "Sorry, but you can't be your own watchman here");
+                return RedirectToAction("WatchmanProfile");
+            }
+
+            var token = this.GetAccessTokenFromCookies();
+            var patientUserProfile = await _userManager.FindByEmailAsync(patientEmail, token);
+
+            var currentWatchmanId = User.FindFirstValue("watchmanIdClaim");
+            if (patientUserProfile == null)
+            {
+                ModelState.AddModelError("", "We can't find patient with this email");
+            }
+            else if (!String.IsNullOrWhiteSpace(currentWatchmanId))
+            {
+                var patient =
+                    await _watchmanPatientService.GetPatientAsync(patientUserProfile.PatientId, token);
+                if (patient != null &&
+                    !(await _watchmanPatientService.IsControlPatient(Guid.Parse(currentWatchmanId), patient.Id, token)))
+                {
+                    await _controlRequestService.CreateControlRequest(Guid.Parse(currentWatchmanId), patient.Id, token);
+                    ModelState.AddModelError("", "Request sent");
+                }
+                else
+                {
+                    ModelState.AddModelError("", "You already monitor that patient");
+                }
+            }
+            return RedirectToAction("WatchmanProfile");
+        }
+
+        public async Task<IActionResult> AcceptRequest(string requestId)
+        {
+            if (String.IsNullOrWhiteSpace(requestId) || !Guid.TryParse(requestId, out var result))
+            {
+                ModelState.AddModelError("", $"Wrong request Id: {requestId}");
+            }
+            else
+            {
+                string token = this.GetAccessTokenFromCookies();
+                await _controlRequestService.UpdateRequestStatus(result, ControlRequestStatus.Accepted, token);
+
+                var request = await _controlRequestService.GetControlRequest(result, token);
+                await _watchmanPatientService.AddPatientToWatchmanAsync(request.WatchmanId, request.PatientId, token);
+            }
+            return RedirectToAction("PatientProfile");
+        }
+
+        public async Task<IActionResult> RefuseRequest(string requestId)
+        {
+            if (String.IsNullOrWhiteSpace(requestId) || !Guid.TryParse(requestId, out var result))
+            {
+                ModelState.AddModelError("", $"Wrong request Id: {requestId}");
+            }
+            else
+            {
+                await _controlRequestService.UpdateRequestStatus(result, ControlRequestStatus.Refused,
+                    this.GetAccessTokenFromCookies());
+            }
+            return RedirectToAction("PatientProfile");
+        }
+
+        public IActionResult BlockRequestFromWatchman(string email)
+        {
+            throw new NotImplementedException();
         }
     }
 }
---
+++ Watchman.Web/ViewModels/PatientProfileViewModel.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Watchman.BusinessLogic.Models.Users;
+using Watchman.Web.Models;
+
+namespace Watchman.Web.ViewModels
+{
+    public class PatientProfileViewModel
+    {
+        public PatientInfo Patient { get; }
+        public IEnumerable<PersonalInfoRequestIdPair> ControlRequests { get; }
+
+        public PatientProfileViewModel(Patient<Guid> patient, IEnumerable<PersonalInfoRequestIdPair> pendingRequests)
+        {
+            this.Patient = patient as PatientInfo;
+            this.ControlRequests = pendingRequests;
+        }
+    }
+
+    public class PersonalInfoRequestIdPair
+    {
+        public PersonalInfo PersonalInformation { get; }
+        public Guid RequestId { get; }
+
+        public PersonalInfoRequestIdPair(PersonalInfo personalInformation, Guid requestId)
+        {
+            PersonalInformation = personalInformation;
+            RequestId = requestId;
+        }
+    }
+}
