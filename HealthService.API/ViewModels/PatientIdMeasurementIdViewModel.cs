ccf47b5f8e29e0f0efaceb925769199c5ff3aa6a
rusgumeniuk <rus.gumeniuk@gmail.com>
2020-04-24 22:36:25 +0300

    create 2 viewmodels

---
+++ HealthService.API/ViewModels/PatientIdMeasurementIdViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+using Watchman.API.Common.Attributes;
+
+namespace HealthService.API.ViewModels
+{
+    public class PatientIdMeasurementIdViewModel
+    {
+        [NotEmptyGuid]
+        public Guid MeasurementId { get; set; }
+        [NotEmptyGuid]
+        public Guid PatientId { get; set; }
+    }
+}
