0e173b2c60d7c0ca2107fb74847c5ab1cff9d265
rusgumeniuk <rus.gumeniuk@gmail.com>
2020-05-04 21:47:29 +0300

    Add HealthStateConverter (closes #10)

---
+++ Watchman.BusinessLogic/Models/Converters/HealthStateConverter.cs
@@ -0,0 +1,45 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+using System;
+
+using Watchman.BusinessLogic.Models.PatientStates.HealthStates;
+
+namespace Watchman.BusinessLogic.Models.Converters
+{
+    class HealthStateConverter : JsonConverter
+    {
+        private static readonly JsonSerializerSettings Settings = new JsonSerializerSettings()
+        { ContractResolver = new SignResolver() };
+
+
+        public override object ReadJson(JsonReader reader, Type objectType, object existingValue, JsonSerializer serializer)
+        {
+            JObject jo = JObject.Load(reader);
+            if (!jo.TryGetValue("healthAuditSecondPeriod", out var val))
+            {
+                val = jo.GetValue("HealthAuditSecondPeriod");
+            }
+            switch ((ushort)val)
+            {
+                case 60:
+                    return JsonConvert.DeserializeObject<NormalHealthState>(jo.ToString(), Settings);
+                case 5:
+                    return JsonConvert.DeserializeObject<ThreateningHealthState>(jo.ToString(), Settings);
+                default:
+                    throw new ArgumentException($"Unknown type {val}");
+            }
+        }
+
+        public override bool CanConvert(Type objectType)
+        {
+            return (objectType == typeof(PatientHealthState<Guid>));
+        }
+
+        public override bool CanWrite => false;
+        public override void WriteJson(JsonWriter writer, object value, JsonSerializer serializer)
+        {
+            throw new NotImplementedException();//canWrite => false so we can't write and don't need to implement
+        }
+    }
+}
--- Watchman.BusinessLogic/Models/PatientStates/HealthStates/PatientHealthState.cs
+++ Watchman.BusinessLogic/Models/PatientStates/HealthStates/PatientHealthState.cs
@@ -1,7 +1,12 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+
+using System;
+
+using Watchman.BusinessLogic.Models.Converters;
 
 namespace Watchman.BusinessLogic.Models.PatientStates.HealthStates
 {
+    [JsonConverter(typeof(PatientActivityConverter))]
     public abstract class PatientHealthState<TKey> : IIdentifiedEntity<TKey>
         where TKey : IEquatable<TKey>
     {
