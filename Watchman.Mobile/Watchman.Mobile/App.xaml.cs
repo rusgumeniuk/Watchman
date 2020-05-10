de841708be08f1a3e305eb19bb7fa8beba7c59b4
rusgumeniuk <rus.gumeniuk@gmail.com>
2020-05-10 18:19:29 +0300

    Create LoginPage and set as MainPage (closes #20 )

--- Watchman.Mobile/Watchman.Mobile/App.xaml.cs
+++ Watchman.Mobile/Watchman.Mobile/App.xaml.cs
@@ -10,7 +10,7 @@ namespace Watchman.Mobile
         {
             InitializeComponent();
 
-            MainPage = new MainPage(); 
+            MainPage = new NavigationPage(new LoginPage());
         }
 
         protected override void OnStart()
---
+++ Watchman.Mobile/Watchman.Mobile/Pages/LoginPage.xaml.cs
@@ -0,0 +1,17 @@
+﻿using Watchman.Mobile.ViewModels;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Watchman.Mobile.Pages
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class LoginPage : ContentPage
+    {
+        public LoginPage()
+        {
+            InitializeComponent();
+            this.BindingContext = new LoginViewModel(this.Navigation);
+        }
+    }
+}
